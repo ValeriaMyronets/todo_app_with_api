223c26219166c16ab9f0ca4f0585c4b5c567c5e2
ValeriaMyronets <valery.mir.11@gmail.com>
2024-04-03 21:44:08 +0300

    clone todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useContext, useEffect } from 'react';
+import { UserWarning } from './UserWarning';
+import { getTodos } from './api/todos';
+import { TodoList } from './components/TodoList';
+import { TodosContext } from './TodosContext';
+import { Header } from './components/Header';
+import { USER_ID } from './constants';
+import { Footer } from './components/Footer';
+import { ErrorComponent } from './components/ErrorComponent';
 
 export const App: React.FC = () => {
+  const { todos, setTodos, setErrorMessage } = useContext(TodosContext);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    getTodos(USER_ID)
+      .then(setTodos)
+      .catch(() => setErrorMessage('Unable to load todos'));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        <TodoList />
+
+        {todos.length !== 0 && (
+          <Footer />
+        )}
+      </div>
+
+      <ErrorComponent />
     </div>
   );
 };
